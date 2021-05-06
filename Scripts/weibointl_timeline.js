adf71bd6eb9d35d39bbe67da9c7d33bbb81740ef
iceparis <80054093+iceparis@users.noreply.github.com>
2021-05-06 21:58:23 +0800

    Update weibointl_timeline.js

--- Scripts/weibointl_timeline.js
+++ Scripts/weibointl_timeline.js
@@ -1,7 +1,7 @@
 /*
 [Script]
-http-response ^https?://weibointl\.api\.weibo\.cn/portal\.php\?a=get_coopen_ads requires-body=1,script-path=https://raw.githubusercontent.com/hancj0528/Script/master/weibointl_launch.js
-http-response ^https?://api\.weibo\.cn/2/(statuses|groups)/(unread_hot_|friends_)?timeline requires-body=1,script-path=https://raw.githubusercontent.com/hancj0528/Script/master/weibointl_timeline.js
+http-response ^https?://weibointl\.api\.weibo\.cn/portal\.php\?a=get_coopen_ads requires-body=1,script-path=https://raw.githubusercontent.com/iceparis/Surge/master/Scripts/weibointl_launch.js
+http-response ^https?://api\.weibo\.cn/2/(statuses|groups)/(unread_hot_|friends_)?timeline requires-body=1,script-path=https://raw.githubusercontent.com/iceparis/Surge/master/Scripts/weibointl_timeline.js
 [MITM]
 hostname = weibointl.api.weibo.cn, api.weibo.cn
 */
