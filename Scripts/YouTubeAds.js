3a0c15796393a6a76b45006fc8ec95c5da60e295
iceparis <80054093+iceparis@users.noreply.github.com>
2021-05-21 15:48:28 +0800

    Create YouTubeAds.js

---
+++ Scripts/YouTubeAds.js
@@ -0,0 +1,6 @@
+if ($request.url.indexOf("&ctier") != -1) {
+  let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A");
+  $done({ response: { status: 307, headers: { Location: url } } });
+} else {
+  $done({})
+}
